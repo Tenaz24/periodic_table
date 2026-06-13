ca6e1b62551ce233ca99369a79b2f0fd4d408ad4
Tenaz24 <cynthianompumelelo149@gmail.com>
2026-06-13 13:44:09 -0400

    fix: connection variable

--- element.sh
+++ element.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
 fi
-# Query by atomic number, symbol, or name
+
 if [[ $1 =~ ^[0-9]+$ ]]; then
   RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE e.atomic_number = $1")
 else
   RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius, t.type FROM elements e JOIN properties p USING(atomic_number) JOIN types t USING(type_id) WHERE e.symbol = '$1' OR e.name = '$1'")
 fi
+
 if [[ -z $RESULT ]]; then
   echo "I could not find that element in the database."
 else
